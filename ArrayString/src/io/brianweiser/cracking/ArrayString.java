af2959e099b3e8bd5201093cd5a020dc728c279d
Brian <iamamthwiz+GitHub@gmail.com>
2014-09-06 23:27:35 -0700

    add allUnique solution

--- ArrayString/src/io/brianweiser/cracking/ArrayString.java
+++ ArrayString/src/io/brianweiser/cracking/ArrayString.java
@@ -1,4 +1,20 @@
 package io.brianweiser.cracking;
 
+import java.util.HashSet;
+
 public class ArrayString {
+	public static boolean allUnique(String str) {
+		HashSet<Character> set = new HashSet<Character>(str.length(), 1);
+		
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (set.contains(c)) {
+				return false;
+			} else {
+				set.add(c);
+			}
+		}
+		
+		return true;
+	}
 }
