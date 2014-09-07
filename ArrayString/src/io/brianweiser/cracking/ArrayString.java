e390ee78cd488fc06f5203fe5b187672ec600163
Brian <iamamthwiz+GitHub@gmail.com>
2014-09-06 23:28:50 -0700

    add allUniqueNM Solution

--- ArrayString/src/io/brianweiser/cracking/ArrayString.java
+++ ArrayString/src/io/brianweiser/cracking/ArrayString.java
@@ -17,4 +17,16 @@ public class ArrayString {
 		
 		return true;
 	}
+	
+	public static boolean allUniqueNM(String str) {
+		for (int i = 0; i < str.length() - 1; i++) {
+			for (int j = i + 1; j < str.length(); j++) {
+				if (str.charAt(i) == str.charAt(j)) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
 }
